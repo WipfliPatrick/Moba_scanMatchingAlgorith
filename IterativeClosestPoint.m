0e03508f252c8f8039035c5758f24015901488a5
WipfliPatrick <50666841+WipfliPatrick@users.noreply.github.com>
2019-05-16 11:39:42 +0200

    Add files via upload

--- IterativeClosestPoint.m
+++ IterativeClosestPoint.m
@@ -3,25 +3,35 @@
 %**************************************************************************
 
 %read data
-X=readtable('ScanFilesNew\Scan000000.dat');             %Fancy GUI needed ^^
+X=readtable('ScanFilesNew\Scan000000.dat');           %Fancy GUI needed ^^
 P=readtable('ScanFilesNew\Scan000001.dat');
 
 
 %translation vector
-%T
 T=[1;1];
 %rotation matrix
-R = zeros(2)
+R=zeros(2);
 
-%minimize summ of the quadrats of the distance of the point pairs
+%minimize sum of the quadrats of the distance of the point pairs
 X=table2array(X);
 P=table2array(P);
-size=max(X);
-matrix_height=X(90);
+matrix_height=X(90);                                %Wofür diese Variabeln?
 matrix_width=max([X(1),X(180)]);
-image=zeros(size*2);
 
-%paint it
+%Creats image of the data-cloud
+[image]=paint_image(X);
+%[image2]=paint_image(P);
+%converts polar data-cloud to a kartesic one as a matrix
+[X_k]=transvert_array(X);
+[P_k]=transvert_array(P);
+%calculates the optimal rotationmatrix and translationvector of the two
+%data-clouds
+[R, T] = optimal_R_and_T(X_k, P_k);
+
+%paint the data-cloud on a kartesic map
+function [image]=paint_image(X)
+size=max(X);
+image=zeros(size*2);
 count=0;
 for i=1:181
     alpha=i-1;
@@ -44,32 +54,62 @@ for i=1:181
     count=count+1;
 end
 imshow(image)
+end
+
+%transverts polar to kartesic koordinates and creates a new matrix
+function [kart_matrix] = transvert_array(X)
+size=max(X);
+count=0;
+kart_matrix=zeros(181,2);
+for i=1:181
+    alpha=i-1;
+    d=X(i);
+    if(d==0)
+        %if there is no distance measured there is no measured point
+        %so no point painting
+        continue;
+    end
+    %transformation polar to kartesic
+    x_from_zero=cosd(alpha)*d;
+    y_from_zero=sind(alpha)*d;
+    %zero is in the middle of the image. so need to calculate the offset
+    if(alpha>90)
+        x_wOffset=round(x_from_zero+size);
+    else
+        x_wOffset=round(size-x_from_zero);
+    end
+    y_wOffset=round(size-y_from_zero);
+    %fills kartesic matrix
+    kart_matrix(i,1)=x_wOffset;
+    kart_matrix(i,2)=y_wOffset;
+    count=count+1;
+end
+end
 
 %mean value of the datafiles
-mue_x=sum(X)/length(X);
-mue_p=sum(P)/length(P);
-W=0;
+function [R, T] = optimal_R_and_T(X_k, P_k)
+mue_x=sum(X_k)/length(X_k);
+mue_p=sum(P_k)/length(P_k);
+W=zeros(2);
 for j=1:181
-    W=W+(X(j)-mue_x)*transpose(P(j)-mue_p);         %No Matrix yet
+    W=W+(X_k(j)-mue_x)*transpose(P_k(j)-mue_p);
 end
-% 
-% for elm=X_transp
-%    print(elm) 
-% end
-
 %Singular value decomposition of matrix W
-[U,E,V] = svd(W)
-
+[U,E,V] = svd(W);
 %Optimal rotationsmatrix R
-R = U * transpose(V)
-
+R = U * transpose(V);
+%Optimaler Translationsvektor T
+T = transpose(mue_x) - R * transpose(mue_p);
 %Get filename of next datafile
+end
+
+%Creates the filename of the next upcoming datafile
 function [filename_new] = get_next_filename(count_index)
    
    %incrementing index of the next datafile
    i = count_index;
    %String splits
-   a = 'ScanFilesNew\Scan0';
+   a = 'ScanFilesNew\Scan';
    b = '0';
    c = num2str(i);
    d = '.dat';
@@ -89,4 +129,3 @@ function [filename_new] = get_next_filename(count_index)
     %Unite editted string splits
     filename_new = [a,b,c,d]  
 end
-
